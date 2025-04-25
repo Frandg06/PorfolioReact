f34e6cc41a61debe4ca9ab5c4a2d1e65e2366479
frandg06 <fdiez86@gmail.com>
2025-04-25 09:38:54 +0200

    🎨 style(ui): corregir clases de estilo en el componente ArrowIcon

--- src/ArrowIcon.jsx
+++ src/ArrowIcon.jsx
@@ -3,12 +3,12 @@ export const ArrowIcon = () => {
     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
       <g fill="none">
         <path
-          className="darr:fill-white fill-black"
+          className="dark:fill-white fill-black"
           d="M5.47 17.47a.75.75 0 1 0 1.06 1.06zm1.06 1.06l12-12l-1.06-1.06l-12 12z"
           opacity=".5"
         />
         <path
-          className="darr:stroke-white stroke-black"
+          className="dark:stroke-white stroke-black"
           strokeLinecap="round"
           strokeLinejoin="round"
           strokeWidth="1.5"
