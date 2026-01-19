d3b12325afb982ea413f2dd32762e8913d4a6798
frandg06 <fdiez86@gmail.com>
2026-01-19 20:03:06 +0100

    feat: implement email sending utility with API integration.

--- src/emails/sendEmail.js
+++ src/emails/sendEmail.js
@@ -1,27 +1,29 @@
 export const sendEmail = async (data) => {
   try {
     const response = await fetch(import.meta.env.VITE_EMAIL_API_URL, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        Authorization: import.meta.env.VITE_EMAIL_API_KEY,
+        "Content-Type": "application/json",
+        "X-API-TOKEN": import.meta.env.VITE_EMAIL_API_KEY,
       },
       body: JSON.stringify(data),
     });
 
     if (!response.ok) {
-      throw new Error('Error al enviar el email');
+      throw new Error("Error al enviar el email");
     }
 
     return {
-      status: 'success',
-      message: 'He recibido su mensaje! Muchas gracias por su interés. Te responderé lo antes posible.',
+      status: "success",
+      message:
+        "He recibido su mensaje! Muchas gracias por su interés. Te responderé lo antes posible.",
     };
     // eslint-disable-next-line
   } catch (error) {
     return {
-      status: 'error',
-      message: 'Ha ocurrido un error al enviar tu mensaje. Por favor, inténtalo de nuevo más tarde.',
+      status: "error",
+      message:
+        "Ha ocurrido un error al enviar tu mensaje. Por favor, inténtalo de nuevo más tarde.",
     };
   }
 };
