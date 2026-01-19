7105cb52f4a30318272cfe25addda81351d5619e
frandg06 <fdiez86@gmail.com>
2026-01-19 19:56:15 +0100

    feat: Add email contact form component.

--- src/Email.jsx
+++ src/Email.jsx
@@ -1,16 +1,16 @@
-import toast from 'react-hot-toast';
-import { Icon } from '@iconify/react/dist/iconify.js';
-import { sendEmail } from './emails/sendEmail';
-import { useState } from 'react';
+import toast from "react-hot-toast";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { sendEmail } from "./emails/sendEmail";
+import { useState } from "react";
 
 export const Email = () => {
   const [loading, setLoading] = useState(false);
 
-  const email = 'fdiez86@gmail.com';
+  const email = "fdiez86@gmail.com";
 
   const handleCopy = () => {
     navigator.clipboard.writeText(email);
-    toast.success('Email copiado al portapapeles');
+    toast.success("Email copiado al portapapeles");
   };
 
   const handleSubmit = async (e) => {
@@ -19,8 +19,8 @@ export const Email = () => {
     const form = new FormData(e.target);
     const data = Object.fromEntries(form.entries());
 
-    if (!data.name || !data.email || !data.content) {
-      toast.error('Por favor, complete todos los campos');
+    if (!data.name || !data.email || !data.message) {
+      toast.error("Por favor, complete todos los campos");
       return;
     }
 
@@ -37,7 +37,10 @@ export const Email = () => {
       </h1>
       <form onSubmit={handleSubmit}>
         <div className="flex gap-2 mb-3">
-          <p id="emailInput" className="w-full p-2 text-gray-400 dark:bg-black border border-gray-600 rounded-xl">
+          <p
+            id="emailInput"
+            className="w-full p-2 text-gray-400 dark:bg-black border border-gray-600 rounded-xl"
+          >
             {email}
           </p>
           <button
@@ -52,7 +55,10 @@ export const Email = () => {
             className="px-4 bg-indigo-500 rounded-xl cursor-pointer hover:scale-105 duration-200 transition-all hover:bg-indigo-600"
           >
             {loading ? (
-              <Icon icon="tabler:loader" className="text-white size-5 animate-spin" />
+              <Icon
+                icon="tabler:loader"
+                className="text-white size-5 animate-spin"
+              />
             ) : (
               <Icon icon="tabler:send" className="text-white size-5" />
             )}
@@ -76,11 +82,11 @@ export const Email = () => {
             />
           </div>
           <textarea
-            id="content"
+            id="message"
             rows="4"
             className="w-full p-2 dark:bg-black border border-gray-600 rounded-xl"
             placeholder="Escribe tu mensaje aquí"
-            name="content"
+            name="message"
             required
           ></textarea>
         </div>
